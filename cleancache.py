65c3314ea8af60ea6a7737da81c9f691f56159dc
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2021-01-22 10:23:24 +0100

    added filter option to cleancache

--- cleancache.py
+++ cleancache.py
@@ -2,14 +2,16 @@
 Delete Files that have mismatching SHA1-dependencies
 
 Usage:
- lgncleancache <folder>... [-I includes ...] [options]
+ lgncleancache <folder>... [-I includes ...] [options] [--filter=<ext>]
 
 Options:
   -I ...                        additional includes
   --dry                         dry run, no files are deleted
+  --filter=<ext>                Filter on which extension to check
 """
 
 import os
+from typing import Optional
 
 from common.sha1mark import sha1search
 from docopt import docopt
@@ -22,15 +24,19 @@ def check(path):
         return sha1search(file.readlines(), searchPaths)
 
 
-def visit_folder(folder):
+def visit_folder(folder, ext: Optional[str]):
     badfiles = []
 
     for root, dirs, files in os.walk(folder):
         for file in files:
+            if ext is not None:
+                if not file.endswith(ext):
+                    continue
+
             if not check(os.path.join(root, file)):
                 badfiles += [os.path.join(root, file)]
         for d in dirs:
-            badfiles += visit_folder(os.path.join(root, d))
+            badfiles += visit_folder(os.path.join(root, d), ext)
 
     return badfiles
 
@@ -40,7 +46,7 @@ if __name__ == '__main__':
     searchPaths += arguments['-I']
 
     for path in arguments['<folder>']:
-        for file in visit_folder(path):
+        for file in visit_folder(path,arguments['--filter']):
             if not arguments['--dry']:
                 os.remove(file)
 
